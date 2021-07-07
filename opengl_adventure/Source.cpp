1b39522c0c1373afc168e04192768d488dc7efb8
marcinbielinski <howlempyrean@gmail.com>
2021-07-07 13:00:36 +0200

    RED

--- opengl_adventure/Source.cpp
+++ opengl_adventure/Source.cpp
@@ -3,20 +3,38 @@
 #include <GLFW/glfw3.h>
 
 // VERTEX shader source to dynamically compile it at run-time
-const char* vertexShaderSource = "#version 450 core\n"
-"layout(location = 0) in vec3 aPos; \n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
-"}\n";
+//const char* vertexShaderSource =    "#version 450 core\n"
+//                                    "layout(location = 0) in vec3 aPos; \n"
+//                                    "void main()\n"
+//                                    "{\n"
+//                                    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
+//                                    "}\n";
+
+const char* vertexShaderSource =    "#version 450 core\n"
+                                    "layout(location = 0) in vec3 aPos; \n"
+                                    "out vec4 vertexColor; \n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "    gl_Position = vec4(aPos, 1.0); \n"
+                                    "    vertexColor = vec4(0.5, 0.0, 0.0, 1.0); \n"
+                                    "}\n";
 
 // FRAGMENT shader source
-const char* fragmentShaderSource = "#version 450 core\n"
-"out vec4 FragColor; \n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
-"}\n";
+//const char* fragmentShaderSource =  "#version 450 core\n"
+//                                    "out vec4 FragColor; \n"
+//                                    "void main()\n"
+//                                    "{\n"
+//                                    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
+//                                    "}\n";
+
+// DARK RED
+const char* fragmentShaderSource =  "#version 450 core\n"
+                                    "out vec4 FragColor; \n"
+                                    "in vec4 vertexColor; \n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "    FragColor = vertexColor; \n"
+                                    "}\n";
 
 // ------------------ VERTICES (ENABLE ONE) --------------------
 // ONE TRIANGLE
@@ -219,9 +237,9 @@ int main() {
         glfwSwapBuffers(window);
     }
     glfwTerminate();
-    int nrAttributes;
-    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+    //int nrAttributes;
+    //glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    //std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
     return 0;
 }
 
